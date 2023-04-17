414ae7b83c3db336861ee6a99e40f5aab65d051a
fre.drk.simon@gmail.com <fre.drk.simon@gmail.com>
2023-04-17 21:02:18 +0200

    - fix static url validation
    Signed-off-by: fre.drk.simon@gmail.com <fre.drk.simon@gmail.com>

--- src/MenuItemTypes/MenuItemStaticURLType.php
+++ src/MenuItemTypes/MenuItemStaticURLType.php
@@ -24,7 +24,6 @@ class MenuItemStaticURLType extends BaseMenuItemType
     {
         return [
             'static_url' => 'required',
-            'name' => 'required',
         ];
     }
     public static function getFields(): array
