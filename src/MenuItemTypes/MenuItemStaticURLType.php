c0fbd30b3bbd9b7f478aa0731752450954a6b773
fre.drk.simon@gmail.com <fre.drk.simon@gmail.com>
2023-04-17 20:17:28 +0200

    - fix static url fields and validation
    Signed-off-by: fre.drk.simon@gmail.com <fre.drk.simon@gmail.com>

--- src/MenuItemTypes/MenuItemStaticURLType.php
+++ src/MenuItemTypes/MenuItemStaticURLType.php
@@ -2,6 +2,8 @@
 
 namespace Outl1ne\MenuBuilder\MenuItemTypes;
 
+use Laravel\Nova\Fields\Text;
+
 class MenuItemStaticURLType extends BaseMenuItemType
 {
     public static function getIdentifier(): string
@@ -18,11 +20,19 @@ class MenuItemStaticURLType extends BaseMenuItemType
     {
         return 'static-url';
     }
-
     public static function getRules(): array
     {
         return [
-            'value' => 'required',
+            'static_url' => 'required',
+            'name' => 'required',
         ];
     }
+    public static function getFields(): array
+    {
+        return [
+            Text::make(__('novaMenuBuilder.staticUrl'), 'static_url')
+                ->sortable()
+        ];
+    }
+
 }
